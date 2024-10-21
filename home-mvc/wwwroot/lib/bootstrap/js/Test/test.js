a4ad2b7d39886b8d68b5fe442e9c87a6bf7bd6af
BaHung203 <hungnb23022003@gmail.com>
2024-10-21 11:49:50 +0700

    .

---
+++ home-mvc/wwwroot/lib/bootstrap/js/Test/test.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    $('#idtab').on('click', function() {
+       
+        window.location.href = 'http://localhost:3000/configure/upload';
+    });
+    
+})
